7e90b10b080898ce0da8490a9ced9fbe65f710ec
philhaynes337 <71613755+philhaynes337@users.noreply.github.com>
2021-02-08 21:57:56 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App/App';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import {BrowserRouter} from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Header />
+      <App />
+      <Footer />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
